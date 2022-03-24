e464006744783d41a5d7e0baf8117b2c6ab26158
md-amir-gauhar <gauharamir15@gmail.com>
2022-03-24 13:51:18 +0530

    fix: css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { ImTwitter } from 'react-icons/im'
 import { BsFacebook, BsLinkedin } from 'react-icons/bs'
 import '../styles/Footer.css'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -8,22 +9,22 @@ const Footer = () => {
       <div className="footer-top">
         <section className="about">
           <span>About</span>
-          <a href="#">Contact Us</a>
-          <a href="#">About Us</a>
-          <a href="#">Press</a>
+          <Link to='#'>Contact Us</Link>
+          <Link to='#'>About Us</Link>
+          <Link to='#'>Press</Link>
         </section>
         <section className="help">
           <span>Help</span>
-          <a href="#">Payment</a>
-          <a href="#">Shipping</a>
-          <a href="#">Cancellation & return</a>
-          <a href="#">FAQ</a>
+          <Link to='#'>Payment</Link>
+          <Link to='#'>Shipping</Link>
+          <Link to='#'>Cancellation & return</Link>
+          <Link to='#'>FAQ</Link>
         </section>
         <section className="social">
           <span>Social</span>
-          <a href="#"><ImTwitter style={{ color: "#1A8CD8" }} /></a>
-          <a href="#"><BsFacebook style={{ color: '#fff' }} /></a>
-          <a href="#"><BsLinkedin style={{ color: '#0077B5' }} /></a>
+          <Link to='#'><ImTwitter style={{ color: "#1A8CD8" }} /></Link>
+          <Link to='#'><BsFacebook style={{ color: '#fff' }} /></Link>
+          <Link to='#'><BsLinkedin style={{ color: '#0077B5' }} /></Link>
         </section>
       </div>
       <div className="footer-bottom">
