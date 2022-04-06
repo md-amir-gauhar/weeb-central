f62d720fdcf500d48408dc21b741b99c375490e8
md-amir-gauhar <gauharamir15@gmail.com>
2022-04-06 22:41:34 +0530

    fix: minor issues

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { AiFillCaretLeft, AiFillCaretRight, AiFillTags, AiOutlineClose } from 'react-icons/ai'
+import { AiFillCaretLeft, AiFillCaretRight, AiOutlineClose } from 'react-icons/ai'
 import { ImSad } from 'react-icons/im'
 import { GiBeachBag } from 'react-icons/gi'
 import Address from '../components/Address'
@@ -24,46 +24,46 @@ const Cart = () => {
             Continue Shopping
           </Link>
         </div>
-  ) : (<>
-    <h2 className="title">My Bag <GiBeachBag /></h2>
-    <div className="checkouts flex">
-      <section className="checkout flex flex-col">
-        <Address />
-        <div className="card-container flex">
-          {
-            cart.map(({ id, name, imgUrl, price, quantity, rating }) => (
-              <div className="checkout-card flex" key={id}>
-                <img
-                  src={imgUrl}
-                  alt="manga"
-                />
-                <AiOutlineClose className='remove-btn' onClick={() => removeFromCart({ id })} />
-                <div className="checkout-card-details flex flex-col">
-                  <p>{name}</p>
-                  <span>₹{price}</span>
-                  <div className="quantity flex align-center">
-                    <span>Quantity:</span>
-                    <div className="quantity-details flex align-center">
-                      <AiFillCaretLeft onClick={() => decreaseQuantity({ id, quantity })} />
-                      <span className="amount">{quantity}</span>
-                      <AiFillCaretRight onClick={() => increaseQuantity({ id, name, imgUrl, price, quantity, rating })} />
+      ) : (<>
+        <h2 className="title">My Bag <GiBeachBag /></h2>
+        <div className="checkouts flex">
+          <section className="checkout flex flex-col">
+            <Address />
+            <div className="card-container flex">
+              {
+                cart.map(({ id, name, imgUrl, price, quantity, rating }) => (
+                  <div className="checkout-card flex" key={id}>
+                    <img
+                      src={imgUrl}
+                      alt="manga"
+                    />
+                    <AiOutlineClose className='remove-btn' onClick={() => removeFromCart({ id })} />
+                    <div className="checkout-card-details flex flex-col">
+                      <p>{name}</p>
+                      <span>₹{price}</span>
+                      <div className="quantity flex align-center">
+                        <span>Quantity:</span>
+                        <div className="quantity-details flex align-center">
+                          <AiFillCaretLeft onClick={() => decreaseQuantity({ id, quantity })} />
+                          <span className="amount">{quantity}</span>
+                          <AiFillCaretRight onClick={() => increaseQuantity({ id, name, imgUrl, price, quantity, rating })} />
+                        </div>
+                      </div>
+
+                      <button onClick={() => addToWishlist({ id, name, imgUrl, rating, price })
+                      }>Move to Wishlist</button>
                     </div>
                   </div>
-
-                  <button onClick={() => addToWishlist({ id, name, imgUrl, rating, price })
-                  }>Move to Wishlist</button>
-                </div>
-              </div>
-            ))
-          }
+                ))
+              }
+            </div>
+          </section>
+          <Payment />
         </div>
-      </section>
-      <Payment />
-    </div>
-  </>
+      </>
 
-  )
-}
+      )
+      }
 
     </main >
   )
